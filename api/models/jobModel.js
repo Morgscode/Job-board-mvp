441709c1663ea824c68e7a70b0a04081e14394a6
Luke Morgan <luke@tjcuk.co.uk>
2022-11-06 19:22:57 +0000

    scaffold job model

--- api/models/jobModel.js
+++ api/models/jobModel.js
@@ -1,4 +1,38 @@
 const mysql = require('mysql2');
 const db = require('../utils/db');
 
-
+const Job = db.sequelize.define(
+  'Job',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    salary: {
+      type: DataTypes.TEXT('tiny'),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    deadline: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    active: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  },
+  {
+    tableName: 'jb_jobs',
+  }
+);
