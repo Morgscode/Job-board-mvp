62e47d9c691a6af8bc273c91a363ccdcb9ee97af
Luke Morgan <luke@tjcuk.co.uk>
2022-11-12 19:22:03 +0000

    better job routing

--- api/routes/jobRoutes.js
+++ api/routes/jobRoutes.js
@@ -7,17 +7,10 @@ const AppError = require('../utils/AppError');
 const router = express.Router();
 
 router.route('/')
-.get(controller._index)
-
-router.route('/job-categories/:id')
-.get(controller.findJobsByCategory);
-
-router.route('/locations/:id')
-.get(controller.findJobsByLocation);
+.get(controller._index);
 
 router.route('/:id')
 .get(controller._find);
-
 
 router.use(catchAsync(auth.protect));
 router.use(catchAsync(auth.jobBoardRecruiter));
@@ -28,5 +21,11 @@ router.route('/')
 router.route('/:id')
 .put(controller._update)
 .delete(controller._delete);
+
+router.route('/job-categories/:id')
+.get(controller.findJobsByCategory);
+
+router.route('/locations/:id')
+.get(controller.findJobsByLocation);
  
 module.exports = router;
