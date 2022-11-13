2aaf5ef5894852ed4f613829a176fe3b117414bb
Luke Morgan <luke@tjcuk.co.uk>
2022-11-13 00:36:26 +0000

    middleware tidy and code cleanup

--- api/controllers/locationController.js
+++ api/controllers/locationController.js
@@ -4,10 +4,12 @@ const NotFoundError = require('../utils/NotFoundError');
 const model = require('../models/locationModel');
 
 const _index = catchAsync(async function (req, res, next) {
-  const locations = await model.Location.findAll({...req.pagination});
+  const locations = await model.Location.findAll({ ...req.pagination });
+
   if (!locations || locations?.length === 0) {
     return next(new NotFoundError("we couldn't find any job locations"));
   }
+
   res.status(200).json({
     status: 'success',
     data: { locations, count: locations.length },
@@ -17,40 +19,55 @@ const _index = catchAsync(async function (req, res, next) {
 const _find = catchAsync(async function (req, res, next) {
   const { id } = req.params;
   const location = await model.Location.findOne({ where: { id } });
+
   if (!location) {
     return next(new NotFoundError("we couldn't find that location"));
   }
+
   res.status(200).json({ status: 'success', data: { location } });
 });
 
 const _create = catchAsync(async function (req, res, next) {
   const location = ({ name, description, active } = req.body);
   const record = await model.Location.create(location);
+
   if (!record) {
     return next(new AppError("we couldn't create that job location", 500));
   }
+
   res.status(201).json({ status: 'success', data: { location: record } });
 });
 
 const _update = catchAsync(async function (req, res, next) {
   const { id } = req.params;
   const { location } = req.body;
+
   if (!location) {
-    return next(new AppError("we couldn't update that location", 400));
+    return next(new AppError('you need to pass in some location details', 400));
   }
+
+  const record = await model.Location.findOne({ where: { id } });
+  if (!record) {
+    return next(new NotFoundError("we couldn't find that location"));
+  }
+
   const updated = await model._update(location, { id });
   if (!updated) {
     return next(new AppError("we couldn't update that location", 500));
   }
+
   res.status(200).json({ status: 'success', data: { updated } });
 });
 
 const _delete = catchAsync(async function (req, res, next) {
   const { id } = req.params;
+
   if (!id) {
     return next(new AppError("we couldn't delete that job", 400));
   }
+
   const deleted = await model.Location.destroy({ where: { id } });
+
   res.status(200).json({ status: 'success', data: { deleted } });
 });
 
